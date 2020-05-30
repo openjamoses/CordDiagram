522c7718030a739f32a8193856827f1c1db6eee9
loaly <65258811+loaly@users.noreply.github.com>
2020-05-30 14:11:14 -0400

    Update 3-chord-diagram.js

--- assets/scripts/3-chord-diagram.js
+++ assets/scripts/3-chord-diagram.js
@@ -1,9 +1,8 @@
 "use strict";
 
-/**
- * File allowing to draw the chord diagram.
- */
-
+// /**
+//  * File allowing to draw the chord diagram.
+//  */
 
 /**
  * Creates the groups for the chord diagram.
@@ -26,7 +25,62 @@ function createGroups(g, data, layout, arc, color, total, formatPercent) {
      - Afficher un élément "title" lorsqu'un groupe est survolé par la souris.
   */
 
+  var groups = g.selectAll("g")
+  .data(layout.groups)
+  .enter()
+  
+  groups.append("path")
+  .attr("id", d => "group" + d.index)
+  .style("fill", d => color(data[d.index].name))
+  .style("stroke", d => color(data[d.index].name))
+  .attr("d", arc)
+  .classed("group", true)
+  .append("svg:title")
+  .text(d => titleGroup(data, d, total, formatPercent)) 
+
+
+  groups.append("text")
+  .attr("dx", 4)
+  .attr("dy", 15)
+  .append("textPath")
+  .classed("text", true)
+  .attr("xlink:href", d => "#group" + d.index) 
+  .text(d => tronc(data[d.index].name))
+  .style("fill", "white")
+  .style("font-size", "12px")
+
 }
+/**
+ * Function that truncates station names «Métro Mont-Royal (Rivard/Mont-Royal)» et «Pontiac / Gilford».
+ * 
+ * @param name    Le nom de la station qui doit etre tronqué si trop long.
+ */
+function tronc(name) {
+  switch(name) {
+    case "Pontiac / Gilford":
+      return "Pontiac"
+    case "Métro Mont-Royal (Rivard/Mont-Royal)":
+      return "Métro Mont-Royal"
+    default:
+      return name
+  }
+}
+
+/**
+ * Returns the text that should appear when the mouse is over a group.
+ * 
+ * @param data            Data from JSON file.
+ * @param d               Data associated with the mouse over chord.
+ * @param total           The total number of trips made for the month of August 2015.
+ * @param formatPercent   Function allowing to correctly format a percentage from a number.
+ */
+function titleGroup(data, d,total, formatPercent) {
+  var name = data[d.index].name
+  var totStation = d3.sum(data[d.index].destinations.map(dest => dest.count))
+  var percent = formatPercent(totStation/total)
+  return name + ": " + percent + " des départs"
+}
+
 
 /**
  * Creates the chords for the chord diagram.
@@ -48,8 +102,35 @@ function createChords(g, data, layout, path, color, total, formatPercent) {
      - Create the diagram's chords with an 80% opacity.
      - Show a "title" element when a chord is hovered by the user's mouse.
   */
-
+  g.selectAll("g")
+    .data(layout)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .classed("chord", true)
+    .style("fill", d => color(data[d.source.index].name))
+    .append("svg:title")
+    .text(d => titleChord(data, d, total, formatPercent))
 }
+
+/**
+ *  Returns the text that should appear when the mouse is over the chord.
+ * 
+ * @param data            Data from JSON file.
+ * @param d               Data associated with the mouse over chord.
+ * @param total           The total number of trips made for the month of August 2015.
+ * @param formatPercent   Function allowing to correctly format a percentage from a number.
+ */
+function titleChord(data, d, total, formatPercent) {
+  var nameChordSource = data[d.source.index].name
+  var nameChordTarget = data[d.target.index].name
+  var percentSource = formatPercent(data[d.source.index].destinations[d.target.index].count/total)
+  var percentTarget = formatPercent(data[d.target.index].destinations[d.source.index].count/total)
+  return nameChordSource + " → " + nameChordTarget + ": " + percentSource + "\n"
+      +  nameChordTarget + " → " + nameChordSource + ": " + percentTarget 
+}
+
+
 
 /**
  * initializes the logic when a chord from the chord diagram is hovered.
@@ -62,5 +143,12 @@ function initializeGroupsHovered(g) {
        The other chords have to drawn with an 10% opacity.
      - Reset the default style for the diagram when the user mouse's leaves the diagram's group.
   */
+  g.selectAll(".group")
+  .on("mouseenter", function(group) {
+  g.selectAll(".chord").classed("fade", function(chord) {
+    return !(group.index === chord.source.index || group.index === chord.target.index)
+  })
+  })
 
 }
+
